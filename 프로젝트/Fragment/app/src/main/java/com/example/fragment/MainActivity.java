92f74aaf30254d9fae489ed5c664e0c3b00831d9
권민정 <kmjdec1203@gmail.com>
2021-01-03 19:26:52 +0900

    [Add] activity, class
    액티비티 및 클래스

---
+++ 프로젝트/Fragment/app/src/main/java/com/example/fragment/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    MainFragment fragment1;
+    MenuFragment fragment2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragment1 = new MainFragment();
+        fragment2 = new MenuFragment();
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+            }
+        });
+
+        Button button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+            }
+        });
+    }
+
+    public void onFragmentChange(int index) {
+        if(index == 0) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+        } else if(index == 1) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+        }
+    }
+}
---
+++ 프로젝트/Fragment/app/src/main/java/com/example/fragment/MainFragment.java
@@ -0,0 +1,43 @@
+package com.example.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+    MainActivity activity;
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        activity = (MainActivity) getActivity();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        // 이제 더이상 엑티비티 참조 안 됨
+        activity = null;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView (@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, false);
+        Button button = rootView.findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.onFragmentChange(1);
+            }
+        });
+        return rootView;
+    }
+}
